74edef02656dbc0607af86514a6ba84cd78a1e04
Dmitriy Solopenkov <mail@solopenkov.ru>
2024-09-17 09:26:37 +0300

    first commit

---
+++ config.sh
@@ -0,0 +1,13 @@
+#1 /bin/bash
+
+set -e
+
+sudo apt update && sudo apt install zsh curl git -y
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+cp zshrc.server ~/.zshrc
+source ~/.zshrc
