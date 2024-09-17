2c0b343a3eb147fe2f4eeec613c94495e4eee5d2
Dmitriy Solopenkov <mail@solopenkov.ru>
2024-09-17 10:12:14 +0300

    Change: debug

--- config.sh
+++ config.sh
@@ -9,5 +9,5 @@ echo y | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/m
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-cp ./server/zshrc.server ~/.zshrc
+cp ./zshrc.server ~/.zshrc
 source ~/.zshrc
