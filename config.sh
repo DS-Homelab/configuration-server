d8900761662c49ca95ab168ef6514ea119dd734e
Dmitriy Solopenkov <mail@solopenkov.ru>
2024-09-17 10:09:48 +0300

    Change: debug

--- config.sh
+++ config.sh
@@ -4,10 +4,10 @@ set -e
 
 sudo apt update && sudo apt install zsh curl git -y
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo y | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-cp zshrc.server ~/.zshrc
+cp ./server/zshrc.server ~/.zshrc
 source ~/.zshrc
